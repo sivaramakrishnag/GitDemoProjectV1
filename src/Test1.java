8afa8b27b20188b1a5ca2d1c5e514d0849c3c6a2
sivaramakrishnag <ramkrishnaraj@gmail.com>
2023-02-06 12:05:54 +0530

    first commit...

---
+++ src/Test1.java
@@ -0,0 +1,20 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+		
+		String hungry = "yes";
+		
+		if(hungry == "yes") 
+			System.out.println("eat");
+			else {
+			System.out.println("work");
+			}
+			
+		}
+
+	}
+
+
